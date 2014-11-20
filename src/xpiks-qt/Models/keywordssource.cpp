cbf89daa17fe63afbdcc3821736947f7ba43c384
Taras Kushnir <kushnirTV@gmail.com>
2014-11-20 09:45:31 +0200

    Added initial sources of qt app

---
+++ src/xpiks-qt/Models/keywordssource.cpp
@@ -0,0 +1,5 @@
+#include "keywordssource.h"
+
+KeywordsSource::KeywordsSource()
+{
+}
---
+++ src/xpiks-qt/Models/keywordssource.h
@@ -0,0 +1,10 @@
+#ifndef KEYWORDSSOURCE_H
+#define KEYWORDSSOURCE_H
+
+class KeywordsSource
+{
+public:
+    KeywordsSource();
+};
+
+#endif // KEYWORDSSOURCE_H
