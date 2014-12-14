8b817ec60a8ac485b95b8f35bb00fd9778296534
Taras Kushnir <kushnirTV@gmail.com>
2014-12-14 18:12:23 +0200

    Added missing clipboard helper

---
+++ src/xpiks-qt/Helpers/clipboardhelper.h
@@ -0,0 +1,27 @@
+#ifndef CLIPBOARDHELPER
+#define CLIPBOARDHELPER
+
+#include <QApplication>
+#include <QClipboard>
+#include <QObject>
+
+namespace Helpers {
+    class ClipboardHelper : public QObject
+    {
+        Q_OBJECT
+    public:
+        explicit ClipboardHelper(QObject *parent = 0) : QObject(parent) {
+            clipboard = QApplication::clipboard();
+        }
+
+        Q_INVOKABLE QString getText() {
+            return clipboard->text(QClipboard::Clipboard);
+        }
+
+    private:
+        QClipboard *clipboard;
+    };
+}
+
+#endif // CLIPBOARDHELPER
+
