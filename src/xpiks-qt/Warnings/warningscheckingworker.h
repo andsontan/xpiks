2b4053741710eb4f6b713f1c03e282d74264d8b2
Taras Kushnir <kushnirTV@gmail.com>
2016-01-07 11:25:43 +0200

    Added missing files

---
+++ src/xpiks-qt/Warnings/warningscheckingworker.h
@@ -0,0 +1,61 @@
+/*
+ * This file is a part of Xpiks - cross platform application for
+ * keywording and uploading images for microstocks
+ * Copyright (C) 2014-2015 Taras Kushnir <kushnirTV@gmail.com>
+ *
+ * Xpiks is distributed under the GNU General Public License, version 3.0
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef WARNINGSCHECKINGWORKER_H
+#define WARNINGSCHECKINGWORKER_H
+
+#include <QObject>
+#include "../Common/itemprocessingworker.h"
+#include "warningsitem.h"
+#include "iwarningscheckable.h"
+
+namespace Models{
+    class SettingsModel;
+}
+
+namespace Warnings {
+    class WarningsCheckingWorker: public QObject, public Common::ItemProcessingWorker<WarningsItem>
+    {
+        Q_OBJECT
+        XPIKS_WORKER
+    protected:
+        virtual bool initWorker();
+        virtual bool processOneItem(WarningsItem *item);
+
+    private:
+        void initValuesFromSettings();
+
+    private:
+        int checkDimensions(IWarningsCheckable *item) const;
+        int checkKeywordsCount(IWarningsCheckable *item) const;
+        int checkDescriptionLength(IWarningsCheckable *item) const;
+        int checkTitleWordsCount(IWarningsCheckable *item) const;
+        int checkSpellCheckErrors(IWarningsCheckable *item) const;
+
+    private:
+        Models::SettingsModel *m_SettingsModel;
+        double m_MinimumMegapixels;
+        int m_MaximumKeywordsCount;
+        int m_MaximumDescriptionLength;
+    };
+}
+
+#endif // WARNINGSCHECKINGWORKER_H
