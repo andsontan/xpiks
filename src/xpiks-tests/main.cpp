c53c9507b33ef3b1168b001e42fdf5abdde22bf0
Taras Kushnir <kushnirTV@gmail.com>
2016-03-04 21:32:39 +0200

    Return code of tests

--- src/xpiks-tests/main.cpp
+++ src/xpiks-tests/main.cpp
@@ -15,26 +15,30 @@
 #include "artworkrepository_tests.h"
 #include "filteredmodel_tests.h"
 
-#define QTEST_CLASS(TestObject, vName) \
+#define QTEST_CLASS(TestObject, vName, result) \
     TestObject vName; \
-    QTest::qExec(&vName, argc, argv); \
+    result = result + QTest::qExec(&vName, argc, argv); \
 
 int main(int argc, char *argv[]) {
     Q_UNUSED(argc);
     Q_UNUSED(argv);
 
-    QTEST_CLASS(EncryptionTests, et);
-    QTEST_CLASS(IndicesToRangesTests, itrt);
-    QTEST_CLASS(AddCommand_Tests, act);
-    QTEST_CLASS(RemoveCommand_Tests, rct);
-    QTEST_CLASS(VectorFileNamesTests, vfnt);
-    QTEST_CLASS(ArtworkMetadataTests, amt);
-    QTEST_CLASS(BasicKeywordsModelTests, bkt);
-    QTEST_CLASS(CombinedModelTests, cmt);
-    QTEST_CLASS(StringHelpersTests, sht);
-    QTEST_CLASS(KeywordValidationTests, kvt);
-    QTEST_CLASS(ArtworkRepositoryTests, art);
-    QTEST_CLASS(FilteredModelTests, fil);
+    int result = 0;
+
+    QTEST_CLASS(EncryptionTests, et, result);
+    QTEST_CLASS(IndicesToRangesTests, itrt, result);
+    QTEST_CLASS(AddCommand_Tests, act, result);
+    QTEST_CLASS(RemoveCommand_Tests, rct, result);
+    QTEST_CLASS(VectorFileNamesTests, vfnt, result);
+    QTEST_CLASS(ArtworkMetadataTests, amt, result);
+    QTEST_CLASS(BasicKeywordsModelTests, bkt, result);
+    QTEST_CLASS(CombinedModelTests, cmt, result);
+    QTEST_CLASS(StringHelpersTests, sht, result);
+    QTEST_CLASS(KeywordValidationTests, kvt, result);
+    QTEST_CLASS(ArtworkRepositoryTests, art, result);
+    QTEST_CLASS(FilteredModelTests, fil, result);
+
     QThread::sleep(1);
-    return 0;
+
+    return result;
 }
