17595b967e5e8b8c8ad2ed231ed09378842c926d
Taras Kushnir <kushnirTV@gmail.com>
2016-05-13 17:33:14 +0300

    Fix for warning in failed test

--- src/xpiks-tests/xpiks-tests-integration/main.cpp
+++ src/xpiks-tests/xpiks-tests-integration/main.cpp
@@ -197,11 +197,11 @@ int main(int argc, char *argv[]) {
             if (testResult == 0) {
                 qInfo("Test %s PASSED", test->testName().toStdString().c_str());
             } else {
-                qWarning("Test %s FAILED", test->testName().toStdString().c_str());
+                qInfo("Test %s FAILED", test->testName().toStdString().c_str());
             }
         }
         catch (...) {
-            qWarning("Test %s CRASHED", test->testName().toStdString().c_str());
+            qInfo("Test %s CRASHED", test->testName().toStdString().c_str());
             result += 1;
         }
 
