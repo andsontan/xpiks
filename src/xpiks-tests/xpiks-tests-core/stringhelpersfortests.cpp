462e92b9642f92c714571c4e27f1f959757e14a7
Taras Kushnir <kushnirTV@gmail.com>
2016-07-20 12:57:13 +0300

    Yet another fix for tests

--- src/xpiks-tests/xpiks-tests-core/stringhelpersfortests.cpp
+++ src/xpiks-tests/xpiks-tests-core/stringhelpersfortests.cpp
@@ -3,30 +3,30 @@
 QString getRandomString(int length, bool exactSize) {
     if (length <= 0) { return QString(); }
     
-    QByteArray qbr;
-    qbr.reserve(length);
-    const char chars[] = "/!@#$% ^&*()_+=|";
-    int charsLength = sizeof(chars) - 1;
+    QString result;
+    result.reserve(length);
+    QLatin1String chars("/!@#$% ^&*()_+=|");
+    int charsLength = chars.size() - 1;
 
     while (length--) {
-        qbr.append('a' + qrand()%26);
+        result.append('a' + qrand()%26);
 
         if (qrand() % 3 == 0) {
-            qbr.append(chars[qrand() % charsLength]);
+            result.append(chars.data()[qrand() % charsLength]);
         }
 
         if (qrand() % 11 == 0) {
-            qbr.append('0' + qrand()%10);
+            result.append('0' + qrand()%10);
         }
 
         if (qrand() % 13 == 0) {
-            qbr.append('A' + qrand()%26);
+            result.append('A' + qrand()%26);
         }
     }
 
     if (exactSize) {
-        qbr.truncate(length);
+        result.truncate(length);
     }
 
-    return QString::fromLatin1(qbr);
+    return result;
 }
