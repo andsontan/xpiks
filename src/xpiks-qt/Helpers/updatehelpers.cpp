b81b74b43d0cbd687f6f0542eee4e89da7558cf8
Taras Kushnir <kushnirTV@gmail.com>
2016-11-23 15:02:45 +0200

    Windows installer launching. related #220

--- src/xpiks-qt/Helpers/updatehelpers.cpp
+++ src/xpiks-qt/Helpers/updatehelpers.cpp
@@ -21,17 +21,66 @@
 
 #include "updatehelpers.h"
 #include <QProcess>
+#include <QStringList>
+#include <QDir>
+#include <QCoreApplication>
+#include <QFileInfo>
+#include "../Common/defines.h"
 
 void launchOSXdmg(const QString &dmgPath) {
-    QStringList args;
-    args << dmgPath;
-    QProcess::startDetached("open", args);
+    QStringList arguments;
+    arguments << dmgPath;
+
+    QProcess::startDetached("open", arguments);
+}
+
+QString getLogFilenameForMinistaller() {
+    QString appDataPath = XPIKS_USERDATA_PATH;
+    if (appDataPath.isEmpty()) {
+        appDataPath = QDir::currentPath();
+    }
+
+    const QString &logFileDir = QDir::cleanPath(appDataPath + QDir::separator() + "logs");
+    QDir logsDir(logFileDir);
+    Q_ASSERT(logsDir.exists());
+
+    QString time = QDateTime::currentDateTimeUtc().toString("ddMMyyyy-hhmmss-zzz");
+    QString logFilename = QString("ministaller-%1.log").arg(time);
+
+    QString logFilePath = logsDir.filePath(logFilename);
+    return logFilePath;
+}
+
+void launchWindowsInstaller(const QString &pathToUpdate) {
+    LOG_DEBUG << "#";
+
+    const QString appDirPath = QCoreApplication::applicationDirPath();
+    QDir appDir(appDirPath);
+    QString ministallerPath = appDir.filePath("ministaller.exe");
+
+    if (!QFileInfo(ministallerPath).exists()) {
+        LOG_WARNING << "Updater not found!" << ministallerPath;
+        return;
+    }
+
+    QString installerLogPath = getLogFilenameForMinistaller();
+    QStringList arguments;
+    arguments << "-force-update" << "-gui" <<
+                 "-install-path" << appDirPath <<
+                 "-l" << installerLogPath <<
+                 "-launch-exe" << "xpiks-qt.exe" <<
+                 "-package-path" << pathToUpdate <<
+                 "-stdout";
+
+    QProcess::startDetached(ministallerPath, arguments);
 }
 
 namespace Helpers {
     void installUpdate(const QString &updatePath) {
-#ifdef Q_OS_OSX
+#if defined(Q_OS_OSX)
         launchOSXdmg(updatePath);
+#elif defined(Q_OS_WIN)
+        launchWindowsInstaller(updatePath);
 #endif
     }
 }
