4c49f471ad287aa7bcdabcd54404db5ef8240c09
Taras Kushnir <kushnirTV@gmail.com>
2016-12-16 12:54:43 +0200

    Further color improvements

--- src/xpiks-qt/QMLExtensions/colorsmodel.cpp
+++ src/xpiks-qt/QMLExtensions/colorsmodel.cpp
@@ -24,9 +24,56 @@
 #include <QVector>
 #include <QString>
 #include <QHash>
-#include "../Common/defines.h"
 
 #include <unordered_map>
+
+#include "../Common/defines.h"
+
+
+#define DEFAULT_DARK_COLOR "defaultDarkColor"
+#define DEFAULT_DARKER_COLOR "defaultDarkerColor"
+#define DEFAULT_CONTROL_COLOR "defaultControlColor"
+#define INACTIVE_CONTROL_COLOR "inactiveControlColor"
+#define WHITE_COLOR "whiteColor"
+
+#define INPUT_BACKGROUND "inputBackgroundColor"
+#define INPUT_INACTIVE_BACKGROUND "inputInactiveBackground"
+#define INPUT_FOREGROUND "inputForegroundColor"
+#define INPUT_INACTIVE_FOREGROUND "inputInactiveForeground"
+#define LABEL_ACTIVE_FOREGROUND "labelActiveForeground"
+#define LABEL_INACTIVE_FOREGROUND "labelInactiveForeground"
+
+#define ARTWORK_BACKGROUND "artworkBackground"
+#define ARTWORK_IMAGE_BACKGROUND "artworkImageBackground"
+#define ARTWORK_MODIFIED_COLOR "artworkModifiedColor"
+#define ARTWORK_SAVED_COLOR "artworkSavedColor"
+#define ARTWORK_ACTIVE_COLOR "artworkActiveColor"
+
+#define LIST_SEPARATOR_COLOR "listSeparatorColor"
+#define LIGHT_COLOR "defaultLightColor"
+#define LIGHT_GRAY_COLOR "defaultLightGrayColor"
+
+#define BUTTON_HOVER_BACKGROUND "buttonHoverBackground"
+#define BUTTON_PRESSED_BACKGROUND "buttonPressedBackground"
+#define BUTTON_PRESSED_FOREGROUND "buttonPressedForeground"
+#define BUTTON_DISABLED_FOREGROUND "buttonDisabledForeground"
+
+#define LINK_CLICKED_COLOR "linkClickedColor"
+
+#define SELECTED_IMAGE_BACKGROUND "selectedImageBackground"
+#define SELECTED_ARTWORK_BACKGROUND "selectedArtworkBackground"
+#define CHECKBOX_CHECKED_COLOR "checkboxCheckedColor"
+
+#define ITEMS_SOURCE_BACKGROUND "itemsSourceBackground"
+#define ITEMS_SOURCE_SELECTED "itemsSourceSelected"
+#define ITEMS_SOURCE_FOREGROUND "itemsSourceForeground"
+
+#define DESTRUCTIVE_COLOR "destructiveColor"
+#define GREEN_COLOR "greenColor"
+
+#define STATUS_BAR_COLOR "statusBarColor"
+#define LEFT_SLIDER_COLOR "leftSliderColor"
+#define POPUP_BACKGROUND_COLOR "popupBackgroundColor"
 
 typedef std::unordered_map<std::string, std::string> ColorsMap;
 
@@ -35,50 +82,50 @@ ColorsMap createBlackTheme() {
 
     BlackTheme["name"] = "black";
 
-    BlackTheme["defaultDarkColor"] = "#1e1e1e";
-    BlackTheme["defaultDarkerColor"] = "#333333";
-    BlackTheme["defaultControlColor"] = "#292929";
-    BlackTheme["inactiveControlColor"] = "#404040";
-    BlackTheme["whiteColor"] = "#ffffff";
+    BlackTheme[DEFAULT_DARK_COLOR] = "#1e1e1e";
+    BlackTheme[DEFAULT_DARKER_COLOR] = "#333333";
+    BlackTheme[DEFAULT_CONTROL_COLOR] = "#292929";
+    BlackTheme[INACTIVE_CONTROL_COLOR] = "#404040";
+    BlackTheme[WHITE_COLOR] = "#ffffff";
 
-    BlackTheme["labelActiveForeground"] = "#c9c9c9";
-    BlackTheme["labelInactiveForeground"] = "#939393";
-    BlackTheme["inputBackgroundColor"] = "#5d5d5d";
-    BlackTheme["inputInactiveBackground"] = "#242424";
-    BlackTheme["inputForegroundColor"] = BlackTheme["whiteColor"]; //"#e6e7e8";
-    BlackTheme["inputInactiveForeground"] = BlackTheme["labelActiveForeground"];
+    BlackTheme[LABEL_ACTIVE_FOREGROUND] = "#c9c9c9";
+    BlackTheme[LABEL_INACTIVE_FOREGROUND] = "#939393";
+    BlackTheme[INPUT_BACKGROUND] = "#5d5d5d";
+    BlackTheme[INPUT_INACTIVE_BACKGROUND] = "#242424";
+    BlackTheme[INPUT_FOREGROUND] = BlackTheme[WHITE_COLOR];
+    BlackTheme[INPUT_INACTIVE_FOREGROUND] = BlackTheme[LABEL_ACTIVE_FOREGROUND];
 
-    BlackTheme["artworkBackground"] = "#181818";
-    BlackTheme["artworkImageBackground"] = "#071215";
-    BlackTheme["artworkModifiedColor"] = "#f49c12";
-    BlackTheme["artworkSavedColor"] = "#435151";
-    BlackTheme["artworkActiveColor"] = "#12b9bc";
+    BlackTheme[ARTWORK_BACKGROUND] = "#181818";
+    BlackTheme[ARTWORK_IMAGE_BACKGROUND] = "#071215";
+    BlackTheme[ARTWORK_MODIFIED_COLOR] = "#f49c12";
+    BlackTheme[ARTWORK_SAVED_COLOR] = "#435151";
+    BlackTheme[ARTWORK_ACTIVE_COLOR] = "#12b9bc";
 
-    BlackTheme["defaultLightColor"] = BlackTheme["inputInactiveForeground"];
-    BlackTheme["defaultLightGrayColor"] = "#D0D0D0";
-    BlackTheme["listSeparatorColor"] = BlackTheme["labelActiveForeground"];
+    BlackTheme[LIGHT_COLOR] = BlackTheme[INPUT_INACTIVE_FOREGROUND];
+    BlackTheme[LIGHT_GRAY_COLOR] = "#D0D0D0";
+    BlackTheme[LIST_SEPARATOR_COLOR] = BlackTheme[LABEL_ACTIVE_FOREGROUND];
 
-    BlackTheme["buttonHoverBackground"] = BlackTheme["artworkActiveColor"];
-    BlackTheme["buttonPressedBackground"] = BlackTheme["defaultControlColor"];
-    BlackTheme["buttonPressedForeground"] = BlackTheme["whiteColor"];
-    BlackTheme["buttonDisabledForeground"] = BlackTheme["inputBackgroundColor"];
+    BlackTheme[BUTTON_HOVER_BACKGROUND] = BlackTheme[ARTWORK_ACTIVE_COLOR];
+    BlackTheme[BUTTON_PRESSED_BACKGROUND] = BlackTheme[DEFAULT_CONTROL_COLOR];
+    BlackTheme[BUTTON_PRESSED_FOREGROUND] = BlackTheme[WHITE_COLOR];
+    BlackTheme[BUTTON_DISABLED_FOREGROUND] = BlackTheme[INPUT_BACKGROUND];
 
-    BlackTheme["linkClickedColor"] = BlackTheme["inputForegroundColor"];
+    BlackTheme[LINK_CLICKED_COLOR] = BlackTheme[WHITE_COLOR];
 
-    BlackTheme["selectedImageBackground"] = BlackTheme["labelInactiveForeground"];
-    BlackTheme["selectedArtworkBackground"] = "#404040";
-    BlackTheme["checkboxCheckedColor"] = "#ffffff";
+    BlackTheme[SELECTED_IMAGE_BACKGROUND] = BlackTheme[LABEL_INACTIVE_FOREGROUND];
+    BlackTheme[SELECTED_ARTWORK_BACKGROUND] = "#404040";
+    BlackTheme[CHECKBOX_CHECKED_COLOR] = "#ffffff";
 
-    BlackTheme["itemsSourceBackground"] = BlackTheme["selectedArtworkBackground"];
-    BlackTheme["itemsSourceSelected"] = BlackTheme["selectedImageBackground"];
-    BlackTheme["itemsSourceForeground"] = BlackTheme["inputInactiveForeground"];
+    BlackTheme[ITEMS_SOURCE_BACKGROUND] = BlackTheme[SELECTED_ARTWORK_BACKGROUND];
+    BlackTheme[ITEMS_SOURCE_SELECTED] = BlackTheme[SELECTED_IMAGE_BACKGROUND];
+    BlackTheme[ITEMS_SOURCE_FOREGROUND] = BlackTheme[INPUT_INACTIVE_FOREGROUND];
 
-    BlackTheme["destructiveColor"] = "#e81313";
-    BlackTheme["greenColor"] = "#2daf02";
+    BlackTheme[DESTRUCTIVE_COLOR] = "#e81313";
+    BlackTheme[GREEN_COLOR] = "#2daf02";
 
-    BlackTheme["statusBarColor"] = "#121212";
-    BlackTheme["leftSliderColor"] = BlackTheme["artworkBackground"];
-    BlackTheme["popupBackgroundColor"] = BlackTheme["defaultControlColor"];
+    BlackTheme[STATUS_BAR_COLOR] = "#121212";
+    BlackTheme[LEFT_SLIDER_COLOR] = BlackTheme[ARTWORK_BACKGROUND];
+    BlackTheme[POPUP_BACKGROUND_COLOR] = BlackTheme[DEFAULT_CONTROL_COLOR];
 
     return BlackTheme;
 }
@@ -88,49 +135,49 @@ ColorsMap createSlateGrayTheme() {
 
     SlateGrayTheme["name"] = "slate gray";
 
-    SlateGrayTheme["defaultDarkColor"] = "#22252c";
-    SlateGrayTheme["defaultDarkerColor"] = "#373a42";
-    SlateGrayTheme["defaultControlColor"] = "#30333b";
-    SlateGrayTheme["whiteColor"] = "#ffffff";
+    SlateGrayTheme[DEFAULT_DARK_COLOR] = "#22252c";
+    SlateGrayTheme[DEFAULT_DARKER_COLOR] = "#373a42";
+    SlateGrayTheme[DEFAULT_CONTROL_COLOR] = "#30333b";
+    SlateGrayTheme[WHITE_COLOR] = "#ffffff";
 
-    SlateGrayTheme["inputBackgroundColor"] = "#7a7e86";
-    SlateGrayTheme["inputInactiveBackground"] = SlateGrayTheme["defaultControlColor"];
-    SlateGrayTheme["inputForegroundColor"] = "#c7ccd3";
-    SlateGrayTheme["inputInactiveForeground"] = SlateGrayTheme["inputBackgroundColor"];
-    SlateGrayTheme["labelInactiveForeground"] = "#474b54";
-    SlateGrayTheme["labelActiveForeground"] = SlateGrayTheme["inputBackgroundColor"];
+    SlateGrayTheme[INPUT_BACKGROUND] = "#7a7e86";
+    SlateGrayTheme[INPUT_INACTIVE_BACKGROUND] = SlateGrayTheme[DEFAULT_CONTROL_COLOR];
+    SlateGrayTheme[INPUT_FOREGROUND] = "#c7ccd3";
+    SlateGrayTheme[INPUT_INACTIVE_FOREGROUND] = SlateGrayTheme[INPUT_BACKGROUND];
+    SlateGrayTheme[LABEL_INACTIVE_FOREGROUND] = "#474b54";
+    SlateGrayTheme[LABEL_ACTIVE_FOREGROUND] = SlateGrayTheme[INPUT_BACKGROUND];
 
-    SlateGrayTheme["artworkBackground"] = SlateGrayTheme["defaultDarkColor"];
-    SlateGrayTheme["artworkImageBackground"] = "#1c2025";
-    SlateGrayTheme["artworkModifiedColor"] = "#f49c12";
-    SlateGrayTheme["artworkSavedColor"] = SlateGrayTheme["inputBackgroundColor"];
-    SlateGrayTheme["artworkActiveColor"] = "#2ec1c3";
+    SlateGrayTheme[ARTWORK_BACKGROUND] = SlateGrayTheme[DEFAULT_DARK_COLOR];
+    SlateGrayTheme[ARTWORK_IMAGE_BACKGROUND] = "#1c2025";
+    SlateGrayTheme[ARTWORK_MODIFIED_COLOR] = "#f49c12";
+    SlateGrayTheme[ARTWORK_SAVED_COLOR] = SlateGrayTheme[INPUT_BACKGROUND];
+    SlateGrayTheme[ARTWORK_ACTIVE_COLOR] = "#2ec1c3";
 
-    SlateGrayTheme["defaultLightColor"] = "#E0E0E0";
-    SlateGrayTheme["defaultLightGrayColor"] = "#D0D0D0";
-    SlateGrayTheme["listSeparatorColor"] = SlateGrayTheme["defaultLightColor"];
+    SlateGrayTheme[LIGHT_COLOR] = "#E0E0E0";
+    SlateGrayTheme[LIGHT_GRAY_COLOR] = "#D0D0D0";
+    SlateGrayTheme[LIST_SEPARATOR_COLOR] = SlateGrayTheme[LIGHT_COLOR];
 
-    SlateGrayTheme["buttonHoverBackground"] = "#12b9bc";
-    SlateGrayTheme["buttonPressedBackground"] = "#41b1b7";
-    SlateGrayTheme["buttonDisabledForeground"] = "#283c3f";
-    SlateGrayTheme["buttonPressedForeground"] = "#283c3f";
+    SlateGrayTheme[BUTTON_HOVER_BACKGROUND] = "#12b9bc";
+    SlateGrayTheme[BUTTON_PRESSED_BACKGROUND] = "#41b1b7";
+    SlateGrayTheme[BUTTON_DISABLED_FOREGROUND] = "#283c3f";
+    SlateGrayTheme[BUTTON_PRESSED_FOREGROUND] = "#283c3f";
 
-    SlateGrayTheme["linkClickedColor"] = SlateGrayTheme["inputForegroundColor"];
+    SlateGrayTheme[LINK_CLICKED_COLOR] = SlateGrayTheme[INPUT_FOREGROUND];
 
-    SlateGrayTheme["selectedImageBackground"] = "#3e434d";
-    SlateGrayTheme["selectedArtworkBackground"] = "#474b54";
-    SlateGrayTheme["checkboxCheckedColor"] = "#ffffff";
+    SlateGrayTheme[SELECTED_IMAGE_BACKGROUND] = "#3e434d";
+    SlateGrayTheme[SELECTED_ARTWORK_BACKGROUND] = "#474b54";
+    SlateGrayTheme[CHECKBOX_CHECKED_COLOR] = "#ffffff";
 
-    SlateGrayTheme["itemsSourceBackground"] = SlateGrayTheme["selectedImageBackground"];
-    SlateGrayTheme["itemsSourceSelected"] = SlateGrayTheme["itemsSourceBackground"];
-    SlateGrayTheme["itemsSourceForeground"] = SlateGrayTheme["inputForegroundColor"];
+    SlateGrayTheme[ITEMS_SOURCE_BACKGROUND] = SlateGrayTheme[SELECTED_IMAGE_BACKGROUND];
+    SlateGrayTheme[ITEMS_SOURCE_SELECTED] = SlateGrayTheme[ITEMS_SOURCE_BACKGROUND];
+    SlateGrayTheme[ITEMS_SOURCE_FOREGROUND] = SlateGrayTheme[INPUT_FOREGROUND];
 
-    SlateGrayTheme["destructiveColor"] = "#A81A1A";
-    SlateGrayTheme["greenColor"] = "#2daf02";
+    SlateGrayTheme[DESTRUCTIVE_COLOR] = "#A81A1A";
+    SlateGrayTheme[GREEN_COLOR] = "#2daf02";
 
-    SlateGrayTheme["statusBarColor"] = SlateGrayTheme["artworkImageBackground"];
-    SlateGrayTheme["leftSliderColor"] = SlateGrayTheme["artworkImageBackground"];
-    SlateGrayTheme["popupBackgroundColor"] = SlateGrayTheme["defaultControlColor"];
+    SlateGrayTheme[STATUS_BAR_COLOR] = SlateGrayTheme[ARTWORK_IMAGE_BACKGROUND];
+    SlateGrayTheme[LEFT_SLIDER_COLOR] = SlateGrayTheme[ARTWORK_IMAGE_BACKGROUND];
+    SlateGrayTheme[POPUP_BACKGROUND_COLOR] = SlateGrayTheme[DEFAULT_CONTROL_COLOR];
 
     return SlateGrayTheme;
 }
@@ -203,50 +250,50 @@ namespace QMLExtensions {
         auto &theme = m_RegisteredThemes[index];
         auto &fallback = getFallbackTheme();
 
-        setDefaultDarkColor(getColor("defaultDarkColor", theme, fallback));
-        setDefaultDarkerColor(getColor("defaultDarkerColor", theme, fallback));
-        setDefaultControlColor(getColor("defaultControlColor",theme, fallback));
-        setInactiveControlColor(getColor("inactiveControlColor", theme, fallback));
-        setWhiteColor(getColor("whiteColor", theme, fallback));
+        setDefaultDarkColor(getColor(DEFAULT_DARK_COLOR, theme, fallback));
+        setDefaultDarkerColor(getColor(DEFAULT_DARKER_COLOR, theme, fallback));
+        setDefaultControlColor(getColor(DEFAULT_CONTROL_COLOR,theme, fallback));
+        setInactiveControlColor(getColor(INACTIVE_CONTROL_COLOR, theme, fallback));
+        setWhiteColor(getColor(WHITE_COLOR, theme, fallback));
 
-        setInputBackgroundColor(getColor("inputBackgroundColor", theme, fallback));
-        setInputInactiveBackground(getColor("inputInactiveBackground", theme, fallback));
-        setInputForegroundColor(getColor("inputForegroundColor", theme, fallback));
-        setInputInactiveForeground(getColor("inputInactiveForeground", theme, fallback));
-        setLabelActiveForeground(getColor("labelActiveForeground", theme, fallback));
-        setLabelInactiveForeground(getColor("labelInactiveForeground", theme, fallback));
+        setInputBackgroundColor(getColor(INPUT_BACKGROUND, theme, fallback));
+        setInputInactiveBackground(getColor(INPUT_INACTIVE_BACKGROUND, theme, fallback));
+        setInputForegroundColor(getColor(INPUT_FOREGROUND, theme, fallback));
+        setInputInactiveForeground(getColor(INPUT_INACTIVE_FOREGROUND, theme, fallback));
+        setLabelActiveForeground(getColor(LABEL_ACTIVE_FOREGROUND, theme, fallback));
+        setLabelInactiveForeground(getColor(LABEL_INACTIVE_FOREGROUND, theme, fallback));
 
-        setArtworkBackground(getColor("artworkBackground", theme, fallback));
-        setArtworkImageBackground(getColor("artworkImageBackground", theme, fallback));
-        setArtworkModifiedColor(getColor("artworkModifiedColor", theme, fallback));
-        setArtworkSavedColor(getColor("artworkSavedColor", theme, fallback));
-        setArtworkActiveColor(getColor("artworkActiveColor", theme, fallback));
+        setArtworkBackground(getColor(ARTWORK_BACKGROUND, theme, fallback));
+        setArtworkImageBackground(getColor(ARTWORK_IMAGE_BACKGROUND, theme, fallback));
+        setArtworkModifiedColor(getColor(ARTWORK_MODIFIED_COLOR, theme, fallback));
+        setArtworkSavedColor(getColor(ARTWORK_SAVED_COLOR, theme, fallback));
+        setArtworkActiveColor(getColor(ARTWORK_ACTIVE_COLOR, theme, fallback));
 
-        setDefaultLightColor(getColor("defaultLightColor", theme, fallback));
-        setDefaultLightGrayColor(getColor("defaultLightGrayColor", theme, fallback));
-        setListSeparatorColor(getColor("listSeparatorColor", theme, fallback));
+        setDefaultLightColor(getColor(LIGHT_COLOR, theme, fallback));
+        setDefaultLightGrayColor(getColor(LIGHT_GRAY_COLOR, theme, fallback));
+        setListSeparatorColor(getColor(LIST_SEPARATOR_COLOR, theme, fallback));
 
-        setButtonHoverBackground(getColor("buttonHoverBackground", theme, fallback));
-        setButtonPressedBackground(getColor("buttonPressedBackground", theme, fallback));
-        setButtonPressedForeground(getColor("buttonPressedForeground", theme, fallback));
-        setButtonDisabledForeground(getColor("buttonDisabledForeground", theme, fallback));
+        setButtonHoverBackground(getColor(BUTTON_HOVER_BACKGROUND, theme, fallback));
+        setButtonPressedBackground(getColor(BUTTON_PRESSED_BACKGROUND, theme, fallback));
+        setButtonPressedForeground(getColor(BUTTON_PRESSED_FOREGROUND, theme, fallback));
+        setButtonDisabledForeground(getColor(BUTTON_DISABLED_FOREGROUND, theme, fallback));
 
-        setLinkClickedColor(getColor("linkClickedColor", theme, fallback));
+        setLinkClickedColor(getColor(LINK_CLICKED_COLOR, theme, fallback));
 
-        setSelectedImageBackground(getColor("selectedImageBackground", theme, fallback));
-        setSelectedArtworkBackground(getColor("selectedArtworkBackground", theme, fallback));
-        setCheckboxCheckedColor(getColor("checkboxCheckedColor", theme, fallback));
+        setSelectedImageBackground(getColor(SELECTED_IMAGE_BACKGROUND, theme, fallback));
+        setSelectedArtworkBackground(getColor(SELECTED_ARTWORK_BACKGROUND, theme, fallback));
+        setCheckboxCheckedColor(getColor(CHECKBOX_CHECKED_COLOR, theme, fallback));
 
-        setItemsSourceBackground(getColor("itemsSourceBackground", theme, fallback));
-        setItemsSourceSelected(getColor("itemsSourceSelected", theme, fallback));
-        setItemsSourceForeground(getColor("itemsSourceForeground", theme, fallback));
+        setItemsSourceBackground(getColor(ITEMS_SOURCE_BACKGROUND, theme, fallback));
+        setItemsSourceSelected(getColor(ITEMS_SOURCE_SELECTED, theme, fallback));
+        setItemsSourceForeground(getColor(ITEMS_SOURCE_FOREGROUND, theme, fallback));
 
-        setDestructiveColor(getColor("destructiveColor", theme, fallback));
-        setGreenColor(getColor("greenColor", theme, fallback));
+        setDestructiveColor(getColor(DESTRUCTIVE_COLOR, theme, fallback));
+        setGreenColor(getColor(GREEN_COLOR, theme, fallback));
 
-        setStatusBarColor(getColor("statusBarColor", theme, fallback));
-        setLeftSliderColor(getColor("leftSliderColor", theme, fallback));
-        setPopupBackgroundColor(getColor("popupBackgroundColor", theme, fallback));
+        setStatusBarColor(getColor(STATUS_BAR_COLOR, theme, fallback));
+        setLeftSliderColor(getColor(LEFT_SLIDER_COLOR, theme, fallback));
+        setPopupBackgroundColor(getColor(POPUP_BACKGROUND_COLOR, theme, fallback));
 
         emit themeChanged();
 
