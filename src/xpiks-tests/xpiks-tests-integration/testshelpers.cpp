8afd09f78715c3e7c05ad1fb1dc0bb838985d369
Taras Kushnir <kushnirTV@gmail.com>
2017-01-14 11:13:20 +0100

    Fix for sleepwait helper

--- src/xpiks-tests/xpiks-tests-integration/testshelpers.cpp
+++ src/xpiks-tests/xpiks-tests-integration/testshelpers.cpp
@@ -1,15 +1,25 @@
 #include "testshelpers.h"
 #include <QThread>
 #include <QCoreApplication>
+#include <QDebug>
 
 void sleepWait(int seconds, const std::function<bool ()> &condition) {
     int times = 0;
+    bool becameTrue = false;
+
     while (times < seconds) {
         if (!condition()) {
             QCoreApplication::processEvents(QEventLoop::AllEvents);
             QThread::sleep(1);
+            times++;
+        } else {
+            qDebug() << "Condition became true in" << times << "try out of" << seconds;
+            becameTrue = true;
+            break;
         }
+    }
 
-        times++;
+    if (!becameTrue) {
+        qDebug() << "Condition never was true";
     }
 }
