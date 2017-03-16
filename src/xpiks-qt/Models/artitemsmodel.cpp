f25f20330997972f69a6600793f80ce094e61b68
Taras Kushnir <kushnirTV@gmail.com>
2017-03-16 16:43:10 +0000

    Force autosave when saving. closes #391

--- src/xpiks-qt/Models/artitemsmodel.cpp
+++ src/xpiks-qt/Models/artitemsmodel.cpp
@@ -389,6 +389,7 @@ namespace Models {
         }
 
         m_CommandManager->writeMetadata(modifiedSelectedArtworks, useBackups);
+        m_CommandManager->saveArtworksBackups(modifiedSelectedArtworks);
     }
 
     ArtworkMetadata *ArtItemsModel::getArtworkMetadata(int index) const {
